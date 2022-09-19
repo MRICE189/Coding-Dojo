cef35530ea6bcb03b3b94a2021b534a2c83b6e91
Matt Rice <matt.rice189@gmail.com>
2022-09-19 12:20:25 -0500

    Added Likes

---
+++ Web Fun/Likes/script.js
@@ -0,0 +1,17 @@
+function addNeilLike() {
+    var likes = document.querySelector('#neil_likes');
+    var likes_num = parseInt(likes.textContent);
+    likes.textContent = likes_num + 1;
+}
+
+function addNicholeLike() {
+    var likes = document.querySelector('#nichole_likes');
+    var likes_num = parseInt(likes.textContent);
+    likes.textContent = likes_num + 1;
+}
+
+function addJimLike() {
+    var likes = document.querySelector('#jim_likes');
+    var likes_num = parseInt(likes.textContent);
+    likes.textContent = likes_num + 1;
+}
