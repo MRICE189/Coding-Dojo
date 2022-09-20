6f87f7d32696b8aa7835947d436b21df064bc5df
Matt Rice <matt.rice189@gmail.com>
2022-09-20 11:57:26 -0500

    Updated DojoWeather

--- Web Fun/DojoWeather/script.js
+++ Web Fun/DojoWeather/script.js
@@ -1,5 +1,3 @@
-var city = "Chicago"
-
 var cities = {
     "chicago" : [
         [80, 65, "sun"],
@@ -21,11 +19,17 @@ var cities = {
     ]
 }
 
-function setCity(element) {
+function cityAlert(element) {
     var city = element.innerText;
     alert(city + " selected.");
     var city_name = document.querySelector("#location h1");
     city_name.innerText = city;
+}
+
+var wantConverted = false;
+
+function setCity(element) {
+    var city = element.innerText;
     var city_temps = null;
     if (city == "Chicago") {
         city_temps = "chicago";
@@ -46,7 +50,6 @@ function setCity(element) {
     los.forEach((temp, index) => {
         temp.textContent = cities[city_temps][index][1];
     })
-    convertTemps();
     weather_pics.forEach((pic, index) => {
         if (cities[city_temps][index][2] == "sun") {
             pic.src = "./assets/some_sun.png";
@@ -69,23 +72,38 @@ function setCity(element) {
             wtext.innerText = "rain showers";
         }
     })
+    convertTemps(wantConverted);
 }
 
 function removePopup(element) {
     element.parentElement.remove();
 }
 
-function convertTemps() {
+function wantConvert() {
+    var wantConverted = true;
+    convertTemps(wantConverted);
+}
+
+function convertTemps(wantConverted) {
+    var isConverted = false;
     var temps_measurement = document.getElementById('measurement').value;
     var temps = document.querySelectorAll('.hi_temp, .lo_temp');
-    if (temps_measurement === "F") {
+    if (temps_measurement == "F") {
+        isConverted = true;
+    }
+    else if (temps_measurement == "C") {
+        isConverted = false;
+        wantConverted = true;
+    }
+    if (isConverted && wantConverted) {
         for (i=0; i<temps.length; i++) {
             var calc_temp = (temps[i].textContent);
             calc_temp = (calc_temp * (9/5) + 32);
             temps[i].innerText = calc_temp.toFixed();
+            wantConverted = false;
         }
     }
-    else if (temps_measurement === "C") {
+    else if (!isConverted && wantConverted) {
         for (i=0; i<temps.length; i++) {
             var calc_temp = (temps[i].textContent);
             calc_temp = (calc_temp - 32) * (5/9);
