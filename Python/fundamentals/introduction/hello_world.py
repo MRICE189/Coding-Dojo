f7fba2b04ef87ae0c948b54c1daa4884b9a5bb77
Matt Rice <matt.rice189@gmail.com>
2022-09-25 12:26:19 -0500

    Added Hello World

---
+++ Python/fundamentals/introduction/hello_world.py
@@ -0,0 +1,17 @@
+print('Hello World')
+
+name = "Matt"
+print('Hello', name)
+
+print('Hello ' + name)
+
+fav_num = 28
+print('Hello', fav_num)
+
+print('Hello ' + str(fav_num))
+
+food1 = "burritos"
+food2 = "sushi"
+print('I love to eat {} and {}.'.format(food1, food2))
+
+print(f'I love to eat {food1} and {food2}.')
