59b69041fcf5651c5637d16a18a5988963d1f558
Matt Rice <matt.rice189@gmail.com>
2022-10-26 12:55:33 -0500

    updated to functional component

--- MERN/React/prop-it-up/src/components/PersonCard.js
+++ MERN/React/prop-it-up/src/components/PersonCard.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 
-class PersonCard extends Component {
-    render() {
-        const {firstName, lastName, age, hair} = this.props.person;
+const PersonCard = props => {
         return (
             <div>
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair color: {hair}</p>
+                <h1>{props.person.lastName}, {props.person.firstName}</h1>
+                <p>Age: {props.person.age}</p>
+                <p>Hair color: {props.person.hair}</p>
             </div>
         );
     }
-}
+
 
 export default PersonCard;
