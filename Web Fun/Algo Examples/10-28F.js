91b9fd08ab345402f1668ca1795e766f3493e97f
Matt Rice <matt.rice189@gmail.com>
2022-10-28 11:06:48 -0500

    daily algos

--- Web Fun/Algo Examples/10-28F.js
+++ Web Fun/Algo Examples/10-28F.js
@@ -20,25 +20,26 @@
  *    deduped.
  */
 
-// function orderedIntersection(sortedA, sortedB) {
-//     let newArr = []
-//     const arrA = [...new Set(sortedA)]
-//     const arrB = [...new Set(sortedB)]
-//     const count = {};
-//     for (let i=0; i<arrA.length; i++) {
-//         (!count[arrA[i]]) ? count[arrA[i]] = 1 : count[arrA[i]]++;
-//     }
-//     for (let j=0; j<arrB.length; j++) {
-//         (!count[arrB[j]]) ? count[arrB[j]] = 1 : count[arrB[j]]++;
-//     }
-//     for (key in count) {
-//         if (count[key] > 1) {
-//             newArr.push(parseInt(key))
-//         }
-//     }
-//     return newArr;
-// }
+function orderedIntersection(sortedA, sortedB) {
+    let newArr = []
+    const arrA = [...new Set(sortedA)]
+    const arrB = [...new Set(sortedB)]
+    const count = {};
+    for (let i=0; i<arrA.length; i++) {
+        (!count[arrA[i]]) ? count[arrA[i]] = 1 : count[arrA[i]]++;
+    }
+    for (let j=0; j<arrB.length; j++) {
+        (!count[arrB[j]]) ? count[arrB[j]] = 1 : count[arrB[j]]++;
+    }
+    for (key in count) {
+        if (count[key] > 1) {
+            newArr.push(parseInt(key))
+        }
+    }
+    return newArr;
+}
 
+//OR
 
 function orderedIntersection(sortedA, sortedB) {
     let newArr = []
