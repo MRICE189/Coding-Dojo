a06f6057ec47e8181745d6417aab198f9eefd717
Matt Rice <matt.rice189@gmail.com>
2022-11-29 16:07:09 -0600

    Added GameDevII

---
+++ CSharp/OOP/GameDevII/Program.cs
@@ -0,0 +1,15 @@
+﻿Melee meleeGuy = new Melee("Fighter");
+
+meleeGuy.Attack();
+meleeGuy.Rage();
+
+Ranged rangeGuy = new Ranged("Ranger");
+
+rangeGuy.Attack();
+rangeGuy.Dash();
+rangeGuy.Attack();
+
+MagicCaster mageGuy = new MagicCaster("Mage");
+
+mageGuy.Attack();
+mageGuy.Heal(meleeGuy);
