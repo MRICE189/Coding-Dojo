bbb7692121b4ebc09cefd086f57fd4938ec8e51b
Matt Rice <matt.rice189@gmail.com>
2022-12-01 16:08:21 -0600

    Added DojoSurveyWithModel

---
+++ CSharp/ASPdotNET Core/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpPost("submit")]
+    public IActionResult Submit(User userInstance)
+    {
+        if (userInstance.Comment == null) 
+        {
+            userInstance.Comment = "No Comment";
+        }
+        return View("Results", userInstance);
+    }
+}
+
---
+++ CSharp/ASPdotNET Core/DojoSurveyWithModel/Models/HomeModel.cs
@@ -0,0 +1,9 @@
+namespace DojoSurveyWithModel.Models;
+
+public class User
+{
+    public string? Name {set;get;}
+    public string? Location {set;get;}
+    public string? Language {set;get;}
+    public string? Comment {set;get;}
+}
