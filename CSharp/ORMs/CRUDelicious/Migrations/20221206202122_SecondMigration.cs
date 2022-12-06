a2dff46f6c1e4b2e4a102c92128616e098df0bef
Matt Rice <matt.rice189@gmail.com>
2022-12-06 16:13:15 -0600

    Added CRUDelicious

---
+++ CSharp/ORMs/CRUDelicious/Migrations/20221206202122_SecondMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDelicious.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Monsters",
+                table: "Monsters");
+
+            migrationBuilder.RenameTable(
+                name: "Monsters",
+                newName: "Dishes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dishes",
+                table: "Dishes",
+                column: "DishId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dishes",
+                table: "Dishes");
+
+            migrationBuilder.RenameTable(
+                name: "Dishes",
+                newName: "Monsters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Monsters",
+                table: "Monsters",
+                column: "DishId");
+        }
+    }
+}
