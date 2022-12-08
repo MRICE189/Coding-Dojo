d8afcd211a4f4485f066aabf493ccfc8f3288e17
Matt Rice <matt.rice189@gmail.com>
2022-12-08 16:31:42 -0600

    updated WeddingPlanner

--- CSharp/ORMs/WeddingPlanner/Models/LoginUser.cs
+++ CSharp/ORMs/WeddingPlanner/Models/LoginUser.cs
@@ -6,9 +6,11 @@ public class LoginUser
 {
     [Required]
     [EmailAddress]
+    [Display(Name = "Email")]
     public string LoginEmail { get; set; }
     [Required]
     [DataType(DataType.Password)]
     [MinLength(8)]
+    [Display(Name = "Password")]
     public string LoginPassword { get; set; }
 }
